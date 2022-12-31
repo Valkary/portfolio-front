6644b2a657fa3ec65ff9ebd13098b32b68f7b645
Pepe <pepoclesng@gmail.com>
2022-12-31 16:53:50 -0600

    fixing some bugs in larger vies

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -18,7 +18,7 @@ export default function About({ }: Props) {
             id="about"
             className="snap-center h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
         >
-            <h3 className="top-24 uppercase tracking-[20px] text-gray-500 text-2xl">About me</h3>
+            <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">About me</h3>
 
             <motion.img
                 initial={{
@@ -32,18 +32,20 @@ export default function About({ }: Props) {
                 transition={{ duration: 1.2 }}
                 viewport={{ once: true }}
                 src="/portfolio-front/about_pic.jpg"
-                className="mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
+                className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
             />
 
-            <div className="space-y-10 px-0 md:px-10">
-                <h4 className="text-4xl font-semibold">Who am I?</h4>
+            <div className="flex flex-col items-center gap-10 px-10">
+                <div className="space-y-10 px-0 md:px-10">
+                    <h4 className="text-4xl font-semibold">Who am I?</h4>
+                </div>
+                <p>
+                    Hi! I'm Pepe Salcedo and I am a fullstack web developer located in México. I started coding when I was about 11 years old with Ruby, but quickly switched
+                    to JavaScript and eventually React, TypeScript and many other technologies. I have a passion for learning new tech and that's the reason why I'm currently
+                    learning Rust and deepening my knowledge in the React and Node ecosystems. I am very keen on <span className="text-blue-300">{text}</span>{<Cursor cursorColor="#f43f5e" />}
+                    apps and keep learning in the process :D.
+                </p>
             </div>
-            <p>
-                Hi! I'm Pepe Salcedo and I am a fullstack web developer located in México. I started coding when I was about 11 years old with Ruby, but quickly switched
-                to JavaScript and eventually React, TypeScript and many other technologies. I have a passion for learning new tech and that's the reason why I'm currently
-                learning Rust and deepening my knowledge in the React and Node ecosystems. I am very keen on <span className="text-blue-300">{text}</span>{<Cursor cursorColor="#f43f5e" />}
-                apps and keep learning in the process :D.
-            </p>
         </motion.section>
     )
 }
