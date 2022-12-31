52beca7520676b02a634452e22295ad3c336d231
Pepe <pepoclesng@gmail.com>
2022-12-31 17:19:34 -0600

    spacing correctly the image on mobile

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -18,7 +18,7 @@ export default function About({ }: Props) {
             id="about"
             className="snap-center h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
         >
-            <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">About me</h3>
+            <h3 className="md:absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">About me</h3>
 
             <motion.img
                 initial={{
@@ -35,7 +35,7 @@ export default function About({ }: Props) {
                 className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
             />
 
-            <div className="flex flex-col items-center gap-10 px-10">
+            <div className="flex flex-col items-center gap-10 md:px-10 mt-10 md:mt-0">
                 <div className="space-y-10 px-0 md:px-10">
                     <h4 className="text-4xl font-semibold">Who am I?</h4>
                 </div>
