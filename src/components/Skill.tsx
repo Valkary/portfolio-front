4f95b469614753fc36e79e6bca5ac85ce275d3bb
Pepe <pepoclesng@gmail.com>
2023-01-11 00:13:53 -0600

    Updating the skills section

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,25 +1,33 @@
-import { motion } from "framer-motion";
+import { useState } from "react";
 
 type Props = {
     icon: JSX.Element;
     experience: string;
+    name: string;
 };
 
-export default function Skill({ icon, experience }: Props) {
+export default function Skill({ icon, experience, name }: Props) {
+    const [hovered, setHovered] = useState(false);
+
     return (
-        <motion.div
-            initial={{
-                opacity: 0.1
-            }}
-            whileHover={{
-                scale: 1.1,
-                opacity: 1
-            }}
-            transition={{ duration: 0.2 }}
-            className="md:w-60 md:h-40 w-35 h-20 rounded-lg bg-[rgba(255,255,255,0.02)] border border-[rgb(255,255,255)] cursor-pointer flex justify-center items-center card"
+        <div
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+            className={`
+                bg-[rgba(255,255,255,0.02)] cursor-pointer flex justify-center items-center relative h-[150px] w-[150px]
+                md:w-64 md:rounded-lg md:opacity-20 md:hover:opacity-100 transition-all duration-300 ${hovered && "md:scale-110"} 
+                rounded-full m-0 p-0 h-full
+            `}
         >
+            <p
+                className={`ease-in-out hidden md:inline-block absolute top-0 left-0 ml-2 mt-2 text-sm tracking-wider text-gray-500 md:opacity-0 transition-all duration-300 ${hovered && "md:opacity-100"}`}
+            >{name}</p>
             {icon}
-            <p className="absolute bottom-0 right-0 mr-2 mb-2 text-sm uppercase tracking-wider text-gray-500">{experience}</p>
-        </motion.div>
+            <p
+                className={`ease-in-out hidden md:inline-block absolute bottom-0 right-0 mr-2 mb-2 text-sm uppercase tracking-wider text-gray-500 md:opacity-0 transition-all duration-300 ${hovered && "md:opacity-100"}`}
+            >{experience}</p>
+        </div>
     );
 }
+
+
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -20,26 +20,27 @@ type Props = {};
 const size = 80;
 
 const skills = [
-    { icon: <ReactOriginalIcon size={size} />, experience: "2 years" },
-    { icon: <MysqlPlainIcon size={size} />, experience: "4 years" },
-    { icon: <NodejsPlainIcon size={size} />, experience: "3 years" },
-    { icon: <TypescriptPlainIcon size={size} />, experience: "1.5 years" },
-    { icon: <TailwindcssPlainIcon size={size} />, experience: "1.5 years" },
-    { icon: <PhpPlainIcon size={size} />, experience: "1 year" },
-    { icon: <JavascriptPlainIcon size={size} />, experience: "5 years" },
-    { icon: <Html5PlainIcon size={size} />, experience: "5 years" },
-    { icon: <Css3PlainIcon size={size} />, experience: "5 years" },
-    { icon: <VscodePlainIcon size={size} />, experience: "4 years" },
-    { icon: <NextjsOriginalWordmarkIcon size={size} color="white" />, experience: "2 years" },
-    { icon: <RustPlainIcon size={size} color={"#CC5500"} />, experience: "> 1 year" }
+    { icon: <ReactOriginalIcon size={size} />, experience: "2 years", name: "React" },
+    { icon: <MysqlPlainIcon size={size} />, experience: "4 years", name: "MySQL" },
+    { icon: <NodejsPlainIcon size={size} />, experience: "3 years", name: "NodeJs" },
+    { icon: <TypescriptPlainIcon size={size} />, experience: "1.5 years", name: "TypeScript" },
+    { icon: <TailwindcssPlainIcon size={size} />, experience: "1.5 years", name: "TailwindCSS" },
+    { icon: <PhpPlainIcon size={size} />, experience: "1 year", name: "PHP" },
+    { icon: <JavascriptPlainIcon size={size} />, experience: "5 years", name: "JavaScript" },
+    { icon: <Html5PlainIcon size={size} />, experience: "5 years", name: "HTML5" },
+    { icon: <Css3PlainIcon size={size} />, experience: "5 years", name: "CSS3" },
+    { icon: <VscodePlainIcon size={size} />, experience: "4 years", name: "Visual Studio Code" },
+    { icon: <NextjsOriginalWordmarkIcon size={size} color="white" />, experience: "2 years", name: "NextJs" },
+    { icon: <RustPlainIcon size={size} color={"#CC5500"} />, experience: "> 1 year", name: "Rust" }
 ];
 
 export default function Skills({ }: Props) {
     return (
         <section id="skills" className="w-full h-screen snap-mandatory snap-center flex flex-col items-center">
-            <h3 className="mt-24 mb-6 uppercase tracking-[20px] text-gray-500 text-2xl">Skills</h3>
-            <div id="cards" className="grid grid-cols-3 justify-center gap-2 m-auto">
-                {skills.map((skill, idx) => <Skill icon={skill.icon} experience={skill.experience} key={idx} />)}
+            <h3 className="md:mb-0 mb-6 mt-24 uppercase tracking-[20px] text-gray-500 text-2xl">Skills</h3>
+            <h3 className="mb-6 uppercase tracking-wide text-gray-600 text-md hidden md:inline-block">Hover over the skills to view the experience</h3>
+            <div id="cards" className="grid md:grid-cols-3 grid-cols-2 justify-center m-auto gap-y-2 gap-x-6 md:gap-x-2">
+                {skills.map((skill, idx) => <Skill icon={skill.icon} experience={skill.experience} name={skill.name} key={idx} />)}
             </div>
         </section>
     );
