bfe2e6ec1d70bf2a4af69bef84880dc2702d846c
Pepe <pepoclesng@gmail.com>
2023-01-25 23:29:00 -0600

    implementing the codeblocks

--- src/components/Blog/CodeBlock.tsx
+++ src/components/Blog/CodeBlock.tsx
@@ -1,27 +1,19 @@
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-
+import { Prism } from "@astrojs/prism";
 type Props = {};
 
-export default function CodeBlock({ }: Props) {
-    const code = `// This library won't stay because it has some weird bugs with the theme imports
-// I guess I'll just code my own library for syntax highlighting
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-// This line just breaks everything for some weird reason
+const code = `import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 const Component = () => {
-  const codeString = '(num) => num + 1';
-  return (
-    <SyntaxHighlighter language="javascript" style={dark}>
-      {codeString}
-    </SyntaxHighlighter>
-  );
+const codeString = '(num) => num + 1';
+return (
+<SyntaxHighlighter language="javascript" style={dark}>
+{codeString}
+</SyntaxHighlighter>
+);
 };`;
 
-    const language = "javascript";
-
+export default function CodeBlock({ }: Props) {
     return (
-        <SyntaxHighlighter language={language}>
-            {code}
-        </SyntaxHighlighter>
+        <Prism lang="js" code={code} />
     );
 }
